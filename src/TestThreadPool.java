5d3fc3fd5e64c2fe8d7d192a9d5739f08f3549c3
miaodada6666 <1005780452@qq.com>
2019-12-02 14:40:27 +0800

    修改了一些注释

--- src/TestThreadPool.java
+++ src/TestThreadPool.java
@@ -3,27 +3,24 @@ import java.util.concurrent.Executors;
 
 public class TestThreadPool{
     public static void main(String[] args){
-        // 1. 创建线程池
+        // 1. 创建线程池,线程池的大小为5
         ExecutorService pool = Executors.newFixedThreadPool(5);
 
         ThreadPoolDemo tpd = new ThreadPoolDemo();
 
         // 2. 为线程池中线程分配任务
-        //    submit(Callable<T> task)
-        //    submit(Runnable task)
-
-        //40为等待放入线程池中的任务的数量大于等于5结果都一样
+        // 40为等待放入线程池中的任务的数量，大于等于5结果都一样
         for(int i=0; i<40; i++){
             pool.submit(tpd);
         }
 
-        // 3. 当线程池中的某一个线程执行结束后，关闭线程池
+        // 3. 当线程池中线程执行任务结束后，关闭线程池
         pool.shutdown();
     }
 }
 
 class ThreadPoolDemo implements Runnable{
-
+    //对于这个类来说是私有的，但是对于线程池中的线程是公共的，还是加锁了的
     private int i=0;
 
     public void run(){
